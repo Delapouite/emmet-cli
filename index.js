0123f564f9ad07ec3f88f88a58527145d1992ed3
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2018-09-17 21:20:35 +0200

    Make placeholders opt-in using a flag

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const { expand } = require('@emmetio/expand-abbreviation')
 const { html: snippets } = require('@emmetio/snippets')
 const field = (index, placeholder) => `\${${index}${placeholder ? ':' + placeholder : ''}}`;
 
+let expand_args = { snippets }
 let abbr = ''
 if (argv[2]) {
-  abbr = argv[2]
-  console.log(expand(abbr, { snippets, field }))
+  if (argv[2] === '-p') {
+      expand_args = {snippets, field}
+      if (argv[3]) {
+          abbr = argv[3]
+      }
+  }
+}
+if (abbr !== '') {
+  console.log(expand(abbr, expand_args))
 } else {
   stdin.setEncoding('utf8')
 
@@ -19,5 +27,5 @@ if (argv[2]) {
     }
   })
 
-  stdin.on('end', () => console.log(expand(abbr, { snippets, field })))
+  stdin.on('end', () => console.log(expand(abbr, expand_args)))
 }
